d62af7572a12658290c35549b5048828b2567b4d
kdsama <kdsama5593@gmail.com>
2023-08-23 18:30:05 +0100

    wrote base test cases for the cache layer, need to put benchmark test cases, but not priority for now

--- repository/cache_test.go
+++ repository/cache_test.go
@@ -43,13 +43,41 @@ func TestLimiterCacheSet(t *testing.T) {
 }
 
 func TestLimiterCacheGet(t *testing.T) {
+	var (
+		limiter = NewLimiterCache()
+		key     = "kdsite/xyz"
+	)
 	type inpS struct {
+		name string
 		url  string
-		obj  *entity.Limiter
 		want error
 	}
 
-	_ = []inpS{
-		{url: "www.kdsite/xyzxas"},
+	testcases := []inpS{
+		{
+			name: "For keys that are not present",
+			url:  "www.kdsite/xyzxas",
+			want: ErrKeyNotFound,
+		},
+	}
+	for _, test := range testcases {
+		t.Run(test.name, func(t *testing.T) {
+			_, got := limiter.Get(test.url)
+			if got != test.want {
+				t.Errorf("Wanted ::%v:: but got ::%v::", test.want, got)
+			}
+		})
+	}
+
+	err := limiter.Set(key, entity.NewLimiter("longerURLForXyz", "something", "kdsite/xyz", utils.OneWeekFromNow()))
+	if err != nil {
+		t.Errorf("Did not expect any error but got %v", err)
+	}
+	obj, err := limiter.Get(key)
+	if err != nil {
+		t.Errorf("Did not expect any error but got %v", err)
+	}
+	if obj.ShortUrl != key {
+		t.Errorf("Wanted %v but got %v ", key, obj.ShortUrl)
 	}
 }
